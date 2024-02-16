184da2d6c880122322b600dbfbcd2e3e540789e5
CarlosFDC <carlosfaundez14@outlook.cl>
2024-02-15 21:02:55 -0300

    Commit con cambios en las migraciones
    -Sincronizada la base de datos postgresql con artisan
    -Creadas migraciones que interactúan con la base de datos creada en pgsql
    
    
    Signed-off-by: CarlosFDC <carlosfaundez14@outlook.cl>

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,13 +12,17 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->id();//integer con incremento
+            $table->string('name',20); //varchar con largo de 20 caracteres
+            $table->text('fullname'); //text, como en postgre
+            $table->string('email')->unique(); //este es unique, basicamente que es unico, no se puede repetir
+            $table->timestamp('email_verified_at')->nullable(); //este es un timestamp, la propiedad nullable es para decir que este campo puede estar vacío
+            $table->string('password');//string de columna llamada password
+            $table->string('avatar');
+            $table->rememberToken();//cada vez que el usuario marque la opcion de "mantener la sesion iniciada" se puede crear esta columna, es un varchar de 
+            //100 de largo, que se encarga de crear un token de inicio de sesion
+            $table->timestamps();//este comando crea 2 columnas llamadas "created_at" y "updated_at", los cuales se utilizan para migraciones
+            //la primera guarda la fecha y hora de un registro, y la 2da es para guardar cuando se actualizó un registro.
         });
     }
 
